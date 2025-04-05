8b87d20a296f6f4fd869054396d6cd6fd067437d
Michał <mkrok3@gmail.com>
2025-04-05 13:22:56 +0200

    Zadanie 4 - instrukcja

---
+++ Spring boot/books/books/src/main/java/pl/edu/pwr/ztw/books/services/IBooksService.java
@@ -0,0 +1,9 @@
+package pl.edu.pwr.ztw.books.services;
+
+import pl.edu.pwr.ztw.books.models.Book;
+
+import java.util.Collection;
+public interface IBooksService {
+    public abstract Collection<Book> getBooks();
+    public abstract Book getBook(int id);
+}
