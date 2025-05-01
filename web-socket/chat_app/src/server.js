0cc164bf75f58c21fd4176c7ce9863584b170e75
Michał <mkrok3@gmail.com>
2025-05-01 12:40:11 +0200

    Dokończona lista 8

--- web-socket/chat_app/src/server.js
+++ web-socket/chat_app/src/server.js
@@ -9,21 +9,76 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client.html');
 });
 
+// Przechowujemy informacje o pokojach i użytkownikach
+const rooms = new Map();
+rooms.set('general', new Set()); // Domyślny pokój
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  let currentUser = { nickname: null, room: null };
+
+  socket.on('join', ({ nickname, room }) => {
+    // Jeśli pokój nie istnieje, utwórz go
+    if (!rooms.has(room)) {
+      rooms.set(room, new Set());
+    }
+
+    // Opuść poprzedni pokój jeśli był
+    if (currentUser.room) {
+      leaveRoom(currentUser.room, currentUser.nickname);
+    }
+
+    // Dołącz do nowego pokoju
+    currentUser = { nickname, room };
+    rooms.get(room).add(nickname);
+    socket.join(room);
+
+    // Powiadom innych w pokoju
+    io.to(room).emit('user joined', nickname);
+    io.to(room).emit('room users', Array.from(rooms.get(room)));
+    
+    // Wyślij aktualną listę pokoi do wszystkich
+    io.emit('room list', Array.from(rooms.keys()));
+    
+    console.log(`${nickname} joined room ${room}`);
+  });
+
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    if (currentUser.room && currentUser.nickname) {
+      leaveRoom(currentUser.room, currentUser.nickname);
+    }
   });
 
   socket.on('chat message', (msg) => {
-    console.log('chat message:', msg);
-    socket.broadcast.emit('chat message', msg);
+    if (currentUser.room) {
+      msg.room = currentUser.room;
+      io.to(currentUser.room).emit('chat message', msg);
+    }
   });
 
-  socket.on('typing', (nickname) => {
-    console.log('typing:', nickname);
-    socket.broadcast.emit('typing', nickname);
+  socket.on('typing', () => {
+    if (currentUser.room && currentUser.nickname) {
+      io.to(currentUser.room).emit('typing', currentUser.nickname);
+    }
   });
+
+  function leaveRoom(room, nickname) {
+    socket.leave(room);
+    rooms.get(room).delete(nickname);
+    
+    // Powiadom innych w pokoju
+    io.to(room).emit('user left', nickname);
+    io.to(room).emit('room users', Array.from(rooms.get(room)));
+    
+    // Jeśli pokój jest pusty, usuń go (oprócz general)
+    if (rooms.get(room).size === 0 && room !== 'general') {
+      rooms.delete(room);
+    }
+    
+    // Wyślij aktualną listę pokoi do wszystkich
+    io.emit('room list', Array.from(rooms.keys()));
+    
+    console.log(`${nickname} left room ${room}`);
+  }
 });
 
 server.listen(3000, () => {
